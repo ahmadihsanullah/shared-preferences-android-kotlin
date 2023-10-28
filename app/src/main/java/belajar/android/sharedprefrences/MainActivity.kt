364ca53b1332b10d7d9739d112061b03e00640d5
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2023-10-28 20:54:26 +0700

    menambah activity user dan membuat layout

---
+++ app/src/main/java/belajar/android/sharedprefrences/MainActivity.kt
@@ -0,0 +1,11 @@
+package belajar.android.sharedprefrences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
